cfdc5c00b0a961bf604a60ce58a0382658b49d87
Lucas Zebre <yakirisk@gmail.com>
2024-08-15 14:47:30 +0200

    can now select the category

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,17 @@ import { useEffect, useState } from "react";
 import {movies$} from "../data/movies"
 import { Movie } from "@/types";
 import MovieCard from "@/components/MovieCard";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 export default function Home() {
 
   const [movies,setMovies]=useState<Movie[]>()
+  const [categorie,setCategorie]=useState<string>("novalue")
+  const [ListCategorie,setListCategorie]=useState<string[]>()
   const [IsLoading,setIsLoading]=useState(false)
   const [error,setError]=useState(false)
 
+
+  // at the mount we fetch all the movies 
   useEffect(()=>{
       movies$.then((data)=>{
         setMovies(data);
@@ -20,6 +25,36 @@ export default function Home() {
         setError(true)
       })
   },[])
+
+
+
+  //  if we delete a movies we need update the list of categorie
+  useEffect(()=>{
+    if(movies){
+      let CategorieList=  movies?.map((movie)=>{
+        return movie.category
+      })
+
+      if(CategorieList){
+        const filteredArray = CategorieList.filter(function(ele, pos) {
+          return CategorieList.indexOf(ele) == pos;
+        })
+
+        setListCategorie(filteredArray)
+      }
+    }
+  },[movies])
+
+
+
+
+
+
+
+
+
+
+
 
   if(IsLoading){
     return (
@@ -40,15 +75,53 @@ export default function Home() {
       return value.id!==id
     })
     setMovies(newMovies)
-  }
+  } 
   
+  function changeCategorie(categorie:string){
+    setCategorie(categorie)
+  }
+
+
+
+
   return (
    <>
-    {movies &&
+   <Select onValueChange={changeCategorie}>
+    <SelectTrigger className="w-[180px]">
+      <SelectValue placeholder="Categorie" />
+    </SelectTrigger>
+    <SelectContent>
+      <SelectItem value={"novalue"}>No categorie</SelectItem>
+      {ListCategorie &&
+          ListCategorie.map((categorie)=>{
+            return     <SelectItem value={categorie}>{categorie}</SelectItem>
+    
+          })
+      }
+    </SelectContent>
+</Select>
+
+{
+  categorie=="novalue" &&
+  <>
+   {movies &&
       movies.map((movie)=>{
         return <MovieCard key={movie.id} id={movie.id} title={movie.title} category={movie.category} deleteMovie={deleteMovie} likes={movie.likes} dislikes={movie.dislikes} posterUrl={movie.posterUrl} />
       })
     }
+  </>
+  ||
+  <>
+       {movies &&
+      movies.filter((movie:Movie)=>{
+        return movie.category == categorie
+      }).map((movie)=>{
+        return <MovieCard key={movie.id} id={movie.id} title={movie.title} category={movie.category} deleteMovie={deleteMovie} likes={movie.likes} dislikes={movie.dislikes} posterUrl={movie.posterUrl} />
+      })
+    }
+  </>
+}
+   
    </>
   );
 }
